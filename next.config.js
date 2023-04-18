f19a0cd32580c057d16af8bc70a309754cb083fc
Vasilena Tsacheva <vasilenatsacheva@gmail.com>
2023-04-18 09:27:08 +0300

    header component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+	reactStrictMode: true,
+};
+module.exports = {
+	...nextConfig,
+	// Add global CSS file to be imported in all components
+	css: ['~/styles/globals.css'],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import type { AppProps } from 'next/app';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../../utils/theme';
+import "@/styles/global.css";
+
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../../utils/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
