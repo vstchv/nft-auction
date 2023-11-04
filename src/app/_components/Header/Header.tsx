e2ef57b6a0392a67ec5031dc3786a8a2c452fc85
Vasilena Tsacheva <vasilenatsacheva@gmail.com>
2023-11-05 00:45:56 +0200

    header done

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -3,32 +3,37 @@ import Logo from "../Logo";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./Header.module.css";
 
-const Header = () => {
+const Header = ({ isLoggedIn = false }) => {
+  const links = [
+    { text: "Home", href: "/" },
+    { text: "Explore", href: "/explore" },
+    { text: "Login", href: "/login" },
+    { text: "Profile", href: "/profile", isVisible: isLoggedIn },
+  ];
+
   return (
-    <>
-      <Grid container className={styles.headerContainer}>
-        <Grid item xs={3} className={styles.gridItem}>
-          <Logo />
-        </Grid>
-        <Grid item xs={5} className={styles.gridItem}>
-          <SearchBar />
-        </Grid>
-        <Grid item xs={4} className={styles.gridItem}>
-          <Button className={styles.links} href="/">
-            Home
-          </Button>
-          <Button className={styles.links} href="/explore">
-            Explore
-          </Button>
-          <Button className={styles.links} href="/login">
-            Login
-          </Button>
-          <Button className={styles.links} href="/login">
-            Profile
-          </Button>
-        </Grid>
+    <Grid container className={styles.headerContainer}>
+      <Grid item xs={3} className={styles.gridItem}>
+        <Logo />
       </Grid>
-    </>
+      <Grid item xs={5} className={styles.gridItem}>
+        <SearchBar />
+      </Grid>
+      <Grid item xs={4} className={styles.gridItem}>
+        {links
+          .filter((link) => link.isVisible !== false)
+          .map((link, index) => (
+            <Button
+              key={index}
+              color="inherit"
+              className={styles.links}
+              href={link.href}
+            >
+              {link.text}
+            </Button>
+          ))}
+      </Grid>
+    </Grid>
   );
 };
 
